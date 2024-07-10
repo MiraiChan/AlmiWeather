57830d8504eb3921c35ab47638699adbe5d79186
Almira Khafizova <mimirka80@gmail.com>
2024-07-10 12:14:53 +0200

    Initial Commit

---
+++ AlmiWeather/AlmiWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlmiWeatherApp.swift
+//  AlmiWeather
+//
+//  Created by Almira Khafizova on 10.07.24.
+//
+
+import SwiftUI
+
+@main
+struct AlmiWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
